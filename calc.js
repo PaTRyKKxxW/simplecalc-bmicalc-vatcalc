040dd246744f217e77e53b872fbfeaa5900aa24a
Patryk Wiktor <patrykkxxw@Patryks-MBP.lan>
2024-02-21 20:18:03 +0100

    finished calculator

--- calc.js
+++ calc.js
@@ -8,3 +8,42 @@ function cleartext(){
     let write = document.getElementById("wypisz");
     write.innerHTML = "";
 }
+function operation(value){
+    let write =  document.getElementById("wypisz");
+    let nowtext = write.innerHTML;
+    if (nowtext.includes('+') || nowtext.includes('-') || nowtext.includes('*') || nowtext.includes('/')){
+        let result = eval(nowtext);
+        write.innerHTML = result + value;
+    }else{
+        write.innerHTML += value;
+    }
+}
+function pierwiastek() {
+    let write = document.getElementById("wypisz");
+    let nowtext = parseFloat(write.innerHTML);
+    let result = Math.sqrt(nowtext);
+    write.innerHTML = result; 
+}
+function potega() {
+    let write = document.getElementById("wypisz");
+    let nowtext = parseFloat(write.innerHTML);
+    let result = Math.pow(nowtext,2);
+    write.innerHTML = result; 
+}
+function proc() {
+    let write = document.getElementById("wypisz");
+    let nowtext = parseFloat(write.innerHTML);
+    let result = nowtext / 100;
+    write.innerHTML = result; 
+}
+function rowna() {
+    let write = document.getElementById("wypisz");
+    let nowtext = write.innerHTML;
+    let lastChar = nowtext[nowtext.length - 1];
+    if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
+        nowtext = nowtext.slice(0, -1);
+    }
+    let result = eval(nowtext);
+    write.innerHTML = result; 
+}
+
