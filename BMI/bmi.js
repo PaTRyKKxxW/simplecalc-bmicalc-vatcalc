d49c49d1fa03fb2069e7540a4f1ed694a41bf9e4
Patryk Wiktor <patrykkxxw@Patryks-MBP.lan>
2024-02-22 20:44:09 +0100

    finish BMI and add folder

--- BMI/bmi.js
+++ BMI/bmi.js
@@ -1,9 +1,9 @@
 let btn = document.querySelector(`#btn`);
 let progress = document.querySelector(`.progress`);
-console.log(progress);
 let InputWidth = document.getElementById(`width`);
 let InputHeight = document.getElementById(`height`);
 let write = document.getElementById(`write`);
+let determination = document.getElementById(`determination`);
 
 let calculate = () =>{
     let width = InputWidth.value;
@@ -14,11 +14,22 @@ let calculate = () =>{
     write.innerHTML = result;
     let resultx5 = result*5;
     progress.style.width = resultx5 + 'px';
+    checkBMI(result);
 };
 
 let checkBMI = (result) =>{
-    if (result<18.5){
-        
+    if (result<18.50){
+        determination.innerHTML = "Mając "+result+" BMI masz niedowage";
+    }else if(result>=18.50 && result<=24.99){
+        determination.innerHTML = "Mając "+result+" BMI masz Wagę prawidłową";
+    }else if(result>=25.00 && result<=29.99){
+        determination.innerHTML = "Mając "+result+" BMI masz nadwagę";
+    }else if(result>=30.0 && result<=34.99){
+        determination.innerHTML = "Mając "+result+" BMI masz I stopień otyłości";
+    }else if(result>=35.00 && result<=39.99){
+        determination.innerHTML = "Mając "+result+" BMI masz II stopień tyłości";
+    }else if(result>=40.00){
+        determination.innerHTML = "Mając "+result+" BMI masz skrajną otyłość";
     }
 }
 btn.addEventListener(`click`,calculate);
