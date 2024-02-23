28edcaf3ec2603607162ee8b7be02faa25448b6b
Patryk Wiktor <patrykkxxw@Patryks-MBP.lan>
2024-02-23 16:36:39 +0100

    prettier

--- BMI/bmi.js
+++ BMI/bmi.js
@@ -5,32 +5,33 @@ let InputHeight = document.getElementById(`height`);
 let write = document.getElementById(`write`);
 let determination = document.getElementById(`determination`);
 
-let calculate = () =>{
-    let width = InputWidth.value;
-    let height = InputHeight.value / 100;
-    height = height * height;
-    let result = width / height;
-    result = Math.round(result * 100)/100;
-    write.innerHTML = result;
-    let resultx5 = result*5;
-    progress.style.width = resultx5 + 'px';
-    checkBMI(result);
+let calculate = () => {
+  let width = InputWidth.value;
+  let height = InputHeight.value / 100;
+  height = height * height;
+  let result = width / height;
+  result = Math.round(result * 100) / 100;
+  write.innerHTML = result;
+  let resultx5 = result * 5;
+  progress.style.width = resultx5 + "px";
+  checkBMI(result);
 };
 
-let checkBMI = (result) =>{
-    if (result<18.50){
-        determination.innerHTML = "Mając "+result+" BMI masz niedowage";
-    }else if(result>=18.50 && result<=24.99){
-        determination.innerHTML = "Mając "+result+" BMI masz Wagę prawidłową";
-    }else if(result>=25.00 && result<=29.99){
-        determination.innerHTML = "Mając "+result+" BMI masz nadwagę";
-    }else if(result>=30.0 && result<=34.99){
-        determination.innerHTML = "Mając "+result+" BMI masz I stopień otyłości";
-    }else if(result>=35.00 && result<=39.99){
-        determination.innerHTML = "Mając "+result+" BMI masz II stopień tyłości";
-    }else if(result>=40.00){
-        determination.innerHTML = "Mając "+result+" BMI masz skrajną otyłość";
-    }
-}
-btn.addEventListener(`click`,calculate);
-
+let checkBMI = (result) => {
+  if (result < 18.5) {
+    determination.innerHTML = "Mając " + result + " BMI masz niedowage";
+  } else if (result >= 18.5 && result <= 24.99) {
+    determination.innerHTML = "Mając " + result + " BMI masz Wagę prawidłową";
+  } else if (result >= 25.0 && result <= 29.99) {
+    determination.innerHTML = "Mając " + result + " BMI masz nadwagę";
+  } else if (result >= 30.0 && result <= 34.99) {
+    determination.innerHTML =
+      "Mając " + result + " BMI masz I stopień otyłości";
+  } else if (result >= 35.0 && result <= 39.99) {
+    determination.innerHTML =
+      "Mając " + result + " BMI masz II stopień tyłości";
+  } else if (result >= 40.0) {
+    determination.innerHTML = "Mając " + result + " BMI masz skrajną otyłość";
+  }
+};
+btn.addEventListener(`click`, calculate);
--- CALCULATOR/calc.js
+++ CALCULATOR/calc.js
@@ -1,71 +1,76 @@
-function results(value){
-   let write =  document.getElementById("wypisz");
-   let nowtext = write.innerHTML;
-   let newtext = nowtext + value;
-   write.innerHTML = newtext;
+function results(value) {
+  let write = document.getElementById("wypisz");
+  let nowtext = write.innerHTML;
+  let newtext = nowtext + value;
+  write.innerHTML = newtext;
 }
-function cleartext(){
-    let write = document.getElementById("wypisz");
-    write.innerHTML = "";
+function cleartext() {
+  let write = document.getElementById("wypisz");
+  write.innerHTML = "";
 }
-function operation(value){
-    let write =  document.getElementById("wypisz");
-    let nowtext = write.innerHTML;
-    if (nowtext.includes('+') || nowtext.includes('-') || nowtext.includes('*') || nowtext.includes('/')){
-        let result = eval(nowtext);
-        write.innerHTML = result + value;
-    }else{
-        write.innerHTML += value;
-    }
+function operation(value) {
+  let write = document.getElementById("wypisz");
+  let nowtext = write.innerHTML;
+  if (
+    nowtext.includes("+") ||
+    nowtext.includes("-") ||
+    nowtext.includes("*") ||
+    nowtext.includes("/")
+  ) {
+    let result = eval(nowtext);
+    write.innerHTML = result + value;
+  } else {
+    write.innerHTML += value;
+  }
 }
 function pierwiastek() {
-    let write = document.getElementById("wypisz");
-    let nowtext = parseFloat(write.innerHTML);
-    let result = Math.sqrt(nowtext);
-    write.innerHTML = result; 
+  let write = document.getElementById("wypisz");
+  let nowtext = parseFloat(write.innerHTML);
+  let result = Math.sqrt(nowtext);
+  write.innerHTML = result;
 }
 function potega() {
-    let write = document.getElementById("wypisz");
-    let nowtext = parseFloat(write.innerHTML);
-    let result = Math.pow(nowtext,2);
-    write.innerHTML = result; 
+  let write = document.getElementById("wypisz");
+  let nowtext = parseFloat(write.innerHTML);
+  let result = Math.pow(nowtext, 2);
+  write.innerHTML = result;
 }
 function proc() {
-    let write = document.getElementById("wypisz");
-    let nowtext = parseFloat(write.innerHTML);
-    let result = nowtext / 100;
-    write.innerHTML = result; 
+  let write = document.getElementById("wypisz");
+  let nowtext = parseFloat(write.innerHTML);
+  let result = nowtext / 100;
+  write.innerHTML = result;
 }
-
-
 
 let history = [];
 
-
 function addToHistory(expression, result) {
-    history.push({ expression: expression, result: result });
+  history.push({ expression: expression, result: result });
 }
 function displayHistory() {
-    let historyContainer = document.getElementById("history");
-    historyContainer.innerHTML = "";
-    history.forEach((item, index) => {
-        let historyItem = document.createElement("p");
-        historyItem.textContent = `${item.expression} = ${item.result}`;
-        historyContainer.appendChild(historyItem);
-    });
+  let historyContainer = document.getElementById("history");
+  historyContainer.innerHTML = "";
+  history.forEach((item, index) => {
+    let historyItem = document.createElement("p");
+    historyItem.textContent = `${item.expression} = ${item.result}`;
+    historyContainer.appendChild(historyItem);
+  });
 }
-
 
 function rowna() {
-    let write = document.getElementById("wypisz");
-    let nowtext = write.innerHTML;
-    let lastChar = nowtext[nowtext.length - 1];
-    if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
-        nowtext = nowtext.slice(0, -1);
-    }
-    let result = eval(nowtext);
-    write.innerHTML = result; 
-    addToHistory(nowtext, result);
-    displayHistory();
+  let write = document.getElementById("wypisz");
+  let nowtext = write.innerHTML;
+  let lastChar = nowtext[nowtext.length - 1];
+  if (
+    lastChar === "+" ||
+    lastChar === "-" ||
+    lastChar === "*" ||
+    lastChar === "/"
+  ) {
+    nowtext = nowtext.slice(0, -1);
+  }
+  let result = eval(nowtext);
+  write.innerHTML = result;
+  addToHistory(nowtext, result);
+  displayHistory();
 }
-
