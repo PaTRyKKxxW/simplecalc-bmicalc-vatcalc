2f970e5fde63190729a922f9308101a9276cb884
Patryk Wiktor <patrykkxxw@Patryks-MBP.lan>
2024-02-23 19:16:36 +0100

    modify prettier

--- VAT/vat.js
+++ VAT/vat.js
@@ -1,36 +1,36 @@
 let btn = document.querySelector(`#btn`);
 
 let calculate = () => {
-    let sum = document.querySelector(`#sum`).value;
-    let vat = document.querySelector(`#VAT`).value;
-    let field = document.querySelector(`#field`).value;
-    let pnetto = document.querySelector(`#pnetto`);
-    let pvat = document.querySelector(`#pvat`);
-    let pbrutto = document.querySelector('#pbrutto');
+  let sum = document.querySelector(`#sum`).value;
+  let vat = document.querySelector(`#VAT`).value;
+  let field = document.querySelector(`#field`).value;
+  let pnetto = document.querySelector(`#pnetto`);
+  let pvat = document.querySelector(`#pvat`);
+  let pbrutto = document.querySelector("#pbrutto");
 
-    let netto,brutto,vatValue;
+  let netto, brutto, vatValue;
 
-    vat = (vat / 100) + 1;
+  vat = vat / 100 + 1;
 
-    if (sum === "netto") {
-        let netto = field;
-        let brutto = field * vat;
-        let vatValue = brutto - netto;
+  if (sum === "netto") {
+    let netto = field;
+    let brutto = field * vat;
+    let vatValue = brutto - netto;
 
-        write(netto, brutto, vatValue);
-    } else if (sum === "brutto") {
-        let brutto = field; 
-        let netto = brutto / vat;
-        let vatValue = brutto - netto;
+    write(netto, brutto, vatValue);
+  } else if (sum === "brutto") {
+    let brutto = field;
+    let netto = brutto / vat;
+    let vatValue = brutto - netto;
 
-        write(netto, brutto, vatValue);
-    }
+    write(netto, brutto, vatValue);
+  }
 };
 
-let write = (netto, brutto, vatValue) =>{
-    pnetto.innerHTML = Math.round(netto * 100) / 100 + " zł";
-    pbrutto.innerHTML = Math.round(brutto * 100) / 100 + " zł";
-    pvat.innerHTML = Math.round(vatValue * 100) / 100 + " zł";
-}
+let write = (netto, brutto, vatValue) => {
+  pnetto.innerHTML = Math.round(netto * 100) / 100 + " zł";
+  pbrutto.innerHTML = Math.round(brutto * 100) / 100 + " zł";
+  pvat.innerHTML = Math.round(vatValue * 100) / 100 + " zł";
+};
 
 btn.addEventListener(`click`, calculate);
